6d1255dc269c26e14f62ca0c0fc3da94e858e46d
Roman Castro <roman.castro@hogia.se>
2023-05-16 22:00:17 +0200

    restrukturerat delar av skriptet plus kommentarer

--- get-azresources_byresourcetype.ps1
+++ get-azresources_byresourcetype.ps1
@@ -1,25 +1,29 @@
-$resourceTypes = @()
+$context = Get-AzContext
+Write-Host "Your current AzContext is:"$context.Subscription.Name
 
-$array = Read-Host "Enter a comma-separated list of resource-itemtypes"
+$array = Read-Host "Enter a list of resource-types separated with commas"
 $itemArray = $array.Split(",")
+
 foreach ($item in $itemArray) {
     
+    $item = $item.Trim("'","""")
+
     $resourceTypes += $item
-    Write-Host "Added: "$item
+    Write-Host "Adding "$item
 }
 
-$subscriptionId = Read-Host "Enter a valid Azure subscriptionId"
-$subscription = Get-AzSubscription -SubscriptionId $subscriptionId
-Write-Host "Accepted: "$subscription.Name
+Write-Host "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
+# $subscriptionId = Read-Host "Enter a valid Azure subscriptionId or press Enter to continue with current"
+$subscriptionId = $context.Subscription.Id
+
+$resourceTypes = @()
 foreach ($type in $resourceTypes) {
-    Write-Host "Getting resources that match the specified type: " $type
-    $result = Get-AzResource -ResourceType $type | Where-Object { 
+    Write-Host "Getting resources that match the specified type:$type"
+    $rsc = Get-AzResource -ResourceType $type | Where-Object { 
         $_.SubscriptionId -eq $subscriptionId 
-    } | Select-Object Name, Location, ResourceGroupName
-	if ($result.Count -eq 0) {
-	    Write-Host "Object returned empty..."
-	else
-            $result.Count
-	}
+    }
 }
+Write-Host "Resources in total:"$rsc.Count
+$rsc | Select-Object Name, Kind, ResourceGroupName | Sort-Object -Property Kind, Name
--- get-azstorageacc_by_usage.ps1
+++ get-azstorageacc_by_usage.ps1
@@ -1,37 +1,56 @@
+Import-Module Az.Storage
+
 $context = Get-AzContext
+# get the current Azure context scope then ask user to confirm
+Write-Host "Your current subscription is:"$context.Subscription.Name
+Read-Host "Press any key to run the script in this context (Ctrl+C to abort)"
+
+# retrive all the storage accounts in the subscription
 $storageAccounts = Get-AzResource -ResourceType 'Microsoft.Storage/storageAccounts'
+
+# create an array to store all the storage accounts
 [System.Collections.ArrayList]$saUsage = New-Object -TypeName System.Collections.ArrayList
 
+# do the following for each storage account in the array (collection)
 foreach ($storageAccount in $storageAccounts) {
     
-    # Get the storage account key (extracts the primary key)
-    ##$saKey = (Get-AzStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.Name).Value[0]
-    # Create a storage context (service-principal object)
-    ##$saContext = New-AzStorageContext -StorageAccountName $storageAccount.Name -StorageAccountKey $saKey
+    # Try to fetch the storage account key (extracts the primary key)
+    try {
+        $saKey = Get-AzStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.Name
+    } 
+    catch {
+        Write-Error "Not able to retrive the Storage Account key at this time."
+        break;
+    }
 
-    # Get the storage service properties for blobs
-    ##$saProp = Get-AzStorageServiceProperty -ServiceType Blob -Context $saContext
+    # Create a new storage context using the primary SA key
+    $saContext = New-AzStorageContext -StorageAccountName $storageAccount.Name -StorageAccountKey $saKey[0].value
 
-    # Get the extended storage service properties
-    $saPropEx = Get-AzStorageBlobServiceProperty -ResourceGroupName $storageAccount.ResourceGroupName -AccountName $storageAccount.Name
+    # Read all of the storage account properties and structure them into objects
+    $saProp = Get-AzStorageServiceProperty -ServiceType Blob -Context $saContext
 
     $StorageAccountDetails = [ordered]@{
-        SubscriptionName = $context.Subscription.Name
-        SubscrpitionID = $context.Subscription.Id
-        StorageAccountName = $storageAccount.Name
-        ResourceGroup = $storageAccount.ResourceGroupName
-        Location = $storageAccount.Location
-        DeleteRetentionPolicy = $saPropEx.DeleteRetentionPolicy.Enabled
-        RetentionPolicyDays = $saPropEx.DeleteRetentionPolicy.Days
-        AllowPermDelete = $saPropEx.DeleteRetentionPolicy.AllowPermanentDelete
-        RestorePolicyEnabled = $saPropEx.RestorePolicy.Enabled
-        RestorePolicyDays= $saPropEx.RestorePolicy.Days
-        MinRestoreTime = $saPropEx.RestorePolicy.MinRestoreTime
-        ChangedFeedEnabled = $saPropEx.ChangeFeed.Enabled
-        ChangeFeedRetention = $saPropEx.ChangeFeed.RetentionInDays
-        VersioningEnabled = $saPropEx.IsVersioningEnabled
-
+        SubscriptionName      = $context.Subscription.Name
+        # SubscrpitionID        = $context.Subscription.Id
+        StorageAccountName    = $storageAccount.Name
+        ResourceGroup         = $storageAccount.ResourceGroupName
+        Location              = $storageAccount.Location
+        AllowPermDelete       = $saProp.DeleteRetentionPolicy.AllowPermanentDelete 
+        DeleteRetentionPolicy = $saProp.DeleteRetentionPolicy.Enabled       
+        DeleteRetentionInDays = $saProp.DeleteRetentionPolicy.RetentionDays
+        # DeleteRetentionPolicy = $saProp.DeleteRetentionPolicy.Enabled
+        # RetentionPolicyDays   = $saProp.DeleteRetentionPolicy.Days
+        # MinRestoreTime        = $saProp.RestorePolicy.MinRestoreTime
+        ChangedFeedEnabled    = $saProp.ChangeFeed.Enabled
+        ChangeFeedRetention   = $saProp.ChangeFeed.RetentionInDays
+        VersioningEnabled     = $saProp.IsVersioningEnabled
+        LoggingOperations     = $saProp.Logging.LoggingOperations           
+        LogRetentionDays      = $saProp.Logging.RetentionDays               
     }
+    
+    # Add each object as a row into the new PSObject serving as the table to output
     $saUsage.add((New-Object psobject -Property $StorageAccountDetails)) | Out-Null
 }
-$saUsage | Export-Excel -Path ".\PSOutputFiles\ListStorageAccProps.xlsx" -WorksheetName "PropertyDetails" -TableName "Table1"
+
+# Write the final table to a formated xls-file
+$saUsage | Export-Excel -Path ".\PSOutputFiles\ListStorageAccProps.xlsx" -WorksheetName "$StorageAccountDetails.SubscriptionName" -TableName "ExportedTable"

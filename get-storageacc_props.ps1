6a6c94a93a51d5c3cedb03fda086a4c244d376fb
Roman Castro <roman.castro@hogia.se>
2023-06-20 00:34:12 +0200

    refactored the output code

--- get-storageacc_props.ps1
+++ get-storageacc_props.ps1
@@ -1,14 +1,16 @@
 <#
 ##############################################################################
-# THIS SCRIPT WILL NOT CHANGE ANY SYSTEM PROPERTIES IN YOUR PROD ENVIRONMENT #
+# NOTE: THIS SCRIPT WILL NOT CHANGE ANY SYSTEM PROPERTIES IN THE ENVIRONMENT #
 ##############################################################################
 Last modified: 2023-05-31 by roman.castro
-(Co-authored with ChatGPT & Github Co-pilot)
+(Co-authored with ChatGPT & Co-pilot)
 #>
 #Run if needed 
-#Import-Module Az.Storage
 
 Write-Host "============= Executing Script - Press Ctrl+C anytime to abort =============" -ForegroundColor Green
+Import-Module Az.Storage
+
+<# A user defined function for prompting the user for a choice from a menu #>
 function New-PromptSelection {
     param ()
     $i = 0; 
@@ -33,15 +35,15 @@ function New-PromptSelection {
     #     Write-Verbose "HelpMessage: $($option.HelpMessage)"
     # }
 
-    # Prompt the user to select a subscription
+    # Draw the menu and prompt user for a selection input
     $title = "=========== USER INPUT REQUIRED ==========="
     $message = "Please select a subscription from the list:"
     $selectedSubscriptionIndex = $host.ui.PromptForChoice($title, $message, $options, -1)
 
-    # Return the selected subscription
+    # Return the user selected subscription
     return $subscriptions[$selectedSubscriptionIndex]
 }
-#user defined function that takes a StorageBlobServiceProperty as input
+<# A user defined function that takes a StorageBlobServiceProperty as input #>
 function New-ExtendedStorageProps {
     [CmdletBinding()]
     param (
@@ -49,7 +51,7 @@ function New-ExtendedStorageProps {
         [System.Object] $obj
     )
 
-    # create new fields with the input from the extended storage properties data
+    # read input object from the extended storage properties data and list properties 
     $saProperties = [ordered] @{
         AllowPermDelete       = $obj.DeleteRetentionPolicy.AllowPermanentDelete
         DeleteRetentionPolicy = $obj.DeleteRetentionPolicy.Enabled
@@ -65,37 +67,38 @@ function New-ExtendedStorageProps {
     Write-Host "." -NoNewline
     return (New-Object PSObject -Property $saProperties)
 }
-# create a list to store the final storage account properties as a "table"
+# create a master list (table) for storing all of the storage account properties
 [System.Collections.ArrayList]$list = New-Object -TypeName System.Collections.ArrayList
 
 $context = Get-AzContext
-Write-Host -ForegroundColor Cyan "The current subscription selected is:"$context.Subscription.Name
-Write-Host "This script requires the appropriate permissions within the environment" -ForegroundColor Yellow
+# call Azure RM and return information about current context to operate
+Write-Host -ForegroundColor Yellow "The current subscription selected is:"$context.Subscription.Name
+Write-Host "This script requires the appropriate permissions within the environment" -ForegroundColor Cyan
 
+# present user a choice to either continue with current context or select a new
 $key = Read-Host "- Do you want to continue to run the script in current context? (Y/n)"
 if ($key -ne "Y") {
-    Write-Host "Loading selection menu..."
+    Write-Host "Loading selection menu..." -ForegroundColor Yellow
     $selection = New-PromptSelection
     $context = Set-AzContext -Subscription $selection.Id
 }
 
+# return all available storage accounts in the selected subscription
 $storageAccounts = Get-AzStorageAccount
 Write-Host "Retrieving and processing storage account properties..."
 foreach ($sa in $storageAccounts) {
-    # skip storage accounts that belong to webjobs
+    # skip all storage accounts that belong to webjobs
     if ($sa.StorageAccountName -notlike "webjob*") {
         Write-Host "Loading"$sa.StorageAccountName -NoNewline
         
-        # create like a "table row"
         $row = New-Object PSObject
-        
-        # add basic primary fields as new columns
+        # populate basic storage information in the fields as row columns
         $row | Add-Member -MemberType NoteProperty -Name 'Subscription' -Value $context.Subscription.Name
         $row | Add-Member -MemberType NoteProperty -Name 'StorageAccount' -Value $sa.StorageAccountName
         $row | Add-Member -MemberType NoteProperty -Name 'ResouceGroup' -Value $sa.ResourceGroupName
         Write-Host "." -NoNewline
 
-        #retrieve the storage account tags (only relevant ones)
+        #retrieve storage account tags (relevant ones)
         $acceptKeys = "company", "team"
         foreach ($key in $sa.Tags.Keys) {
             $value = $sa.Tags[$key]
@@ -103,8 +106,7 @@ foreach ($sa in $storageAccounts) {
                 $row | Add-Member -MemberType NoteProperty -Name $key -Value $value
             }
         }
-
-        #add additional storage account properties (as many as you like)
+        #add additional storage account properties per row (change as needed)
         $row | Add-Member -MemberType NoteProperty -Name 'Type' -Value $sa.Kind
         $row | Add-Member -MemberType NoteProperty -Name 'AccessTier' -Value $sa.AccessTier
         $row | Add-Member -MemberType NoteProperty -Name 'SKU' -Value $sa.Sku.Name
@@ -112,7 +114,7 @@ foreach ($sa in $storageAccounts) {
         ##$row | Add-Member -MemberType NoteProperty -Name 'GeoReplicatedIn' -Value $sa.SecondaryLocation
         Write-Host "." -NoNewline
 
-        #call custom defined function to retrieve the extended properties
+        #call custom defined function to retrieve extended properties for each account
         $ext = New-ExtendedStorageProps(Get-AzStorageBlobServiceProperty -StorageAccount $sa)
         $ext.PSObject.Properties | ForEach-Object {
             $row | Add-Member -MemberType NoteProperty -Name $_.Name -Value $_.Value
@@ -124,7 +126,7 @@ foreach ($sa in $storageAccounts) {
         $row | Add-Member -MemberType NoteProperty -Name 'AllowSharedKey' -Value $sa.AllowSharedKeyAccess
         $row | Add-Member -MemberType NoteProperty -Name 'AllowCrossTenant' -Value $sa.AllowCrossTenantReplication
 
-        # join all fields into a table row
+        # add row to the master list (table)
         $list.Add($row) | Out-Null
         Write-Host " OK "
     }
@@ -137,22 +139,22 @@ Write-Host $list.Count"storage accounts processed. ($skipCount skipped)" -Foregr
 #$properties = $list | ForEach-Object { $_.PSObject.Properties.Name } | Select-Object -Unique
 #$arrayList = $arrayList | Select-Object $properties
 
-$key = Read-Host "- Save output to an Excel file? Choose no to only show the gridview (Y/n)"
-if ($key -ne "Y") {
-    $list | Out-GridView -Title "StorageAccountProperties"
-    return
-}
+$key = Read-Host "- Save output to a file? Choose No to only show Gridview (Y/n)"
+if ($key -eq "Y") {
+        
+    # Output table to excel file and display grid-view (make sure to include file-path and extension)
+    $path = ".\PSOutputFiles\StorageAccProps.xlsx"
 
-# Output table to excel file and display grid-view (make sure to include file-path and extension)
-$path = ".\PSOutputFiles\StorageAccProps.xlsx"
+    try {
+        $list | Export-Excel -Path $path -WorksheetName "ExtendedProperties" -TableName "storageprops" -AutoSize
+    } catch {
+        Write-Error $_.Exception.GetType().FullName
+        Write-Host -ForegroundColor Yellow "Possible reason: Excel file already open? (locked)"
+        return
+    } finally {
+        Write-Host "Script finished successfully. Output can be found under the \PSOutputFiles folder" -ForegroundColor Green
+    }
 
-try {
-    $list | Export-Excel -Path $path -WorksheetName "ExtendedProperties" -TableName "storageprops" -AutoSize
-} catch {
-    Write-Error $_.Exception.GetType().FullName
-    Write-Error "Possible reason: Do you have the Excel file open?"
-    return
-} finally {
+} else {
     $list | Out-GridView -Title "StorageAccountProperties"
-    Write-Host "Script finished successfully. Output can be found under the \PSOutputFiles folder" -ForegroundColor Green
 }

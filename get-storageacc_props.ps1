ca7a27e87ab0c31c00e8a5d2864630f9bb3348df
Roman Castro <roman.castro@hogia.se>
2023-06-20 16:30:43 +0200

    updated help/descriptions for each section

--- get-storageacc_props.ps1
+++ get-storageacc_props.ps1
@@ -6,7 +6,7 @@ Last modified: 2023-05-31 by roman.castro
 (Co-authored with ChatGPT & Co-pilot)
 #
 #>
-
+Clear-Host
 Write-Host "============= Executing Script - Press Ctrl+C anytime to abort =============" -ForegroundColor Green
 Import-Module Az.Storage
 
@@ -14,7 +14,7 @@ Import-Module Az.Storage
 function New-PromptSelection {
     param ()
     $i = 0; 
-    # Create an array list and populate with subscription titles 
+    # Creates a list with all accessible Azure subscriptions 
     $subscriptions = New-Object System.Collections.ArrayList
     foreach ($line in Get-AzSubscription | Select-Object Name, Id) {
         $line | Add-Member NoteProperty -Name Index -Value $i
@@ -22,25 +22,18 @@ function New-PromptSelection {
         $i++
     }
 
-    # Create options for the user to select from
+    # Creates the option list for the user to select a subscription from
     $options = [System.Management.Automation.Host.ChoiceDescription[]]($subscriptions | ForEach-Object {
-            $label = "&$($_.Index) $($_.Name) | "
+            $label = "&$($_.Index) $($_.Name) |"
             New-Object System.Management.Automation.Host.ChoiceDescription $label, $_.Name
         })
 
-    # Display verbose output for debugging
-    # Write-Verbose "Options:"
-    # foreach ($option in $options) {
-    #     Write-Verbose "Label: $($option.Label)"
-    #     Write-Verbose "HelpMessage: $($option.HelpMessage)"
-    # }
-
-    # Draw the menu and prompt user for a selection input
-    $title = "=========== USER INPUT REQUIRED ==========="
-    $message = "Please select a subscription from the list:"
+    # Draws the console menu and prompts user for a selection
+    $title =   "Please select a subscription from the list"
+    $message = "=========================================="
     $selectedSubscriptionIndex = $host.ui.PromptForChoice($title, $message, $options, -1)
 
-    # Return the user selected subscription
+    # Returns the selected subscription
     return $subscriptions[$selectedSubscriptionIndex]
 }
 <# A user defined function for reading the StorageBlobServiceProperty as input #>
@@ -50,8 +43,8 @@ function New-ExtendedStorageProps {
         [Parameter(Mandatory=$true)]
         [System.Object] $obj
     )
-
-    # read input object from the extended storage properties data and list properties 
+    Write-Host "..." -NoNewline
+    # reads the input from the extended storage properties and selects specific properties 
     $saProperties = [ordered] @{
         AllowPermDelete       = $obj.DeleteRetentionPolicy.AllowPermanentDelete
         DeleteRetentionPolicy = $obj.DeleteRetentionPolicy.Enabled
@@ -64,42 +57,40 @@ function New-ExtendedStorageProps {
         ChangedFeedEnabled    = $obj.ChangeFeed.Enabled
         VersioningEnabled     = $obj.IsVersioningEnabled
     }
-    Write-Host "..." -NoNewline
     return (New-Object PSObject -Property $saProperties)
 }
-# create a master list (table) for storing all of the storage account properties
-[System.Collections.ArrayList]$list = New-Object -TypeName System.Collections.ArrayList
 
 $context = Get-AzContext
-# call Azure RM and return information about current context to operate
+# calls Azure RM and returns information about current context
 Write-Host -ForegroundColor Yellow "The current subscription is:"$context.Subscription.Name
-Write-Host "NB! This script requires the appropriate permissions" -ForegroundColor Cyan
+Write-Host "NB! This script requires appropriate permissions to the env." -ForegroundColor Cyan
 
-# present user a choice to either continue with current context or select a new
+# presents user with a choice to either continue with current context or select a new
 $key = Read-Host "- Do you want to continue to run the script in current context? (Y/n)"
 if ($key -ne "Y") {
-    Write-Host "Loading selection menu..." -ForegroundColor Yellow
+    Write-Host "Loading selection menu..." -ForegroundColor Cyan
     $selection = New-PromptSelection
     $context = Set-AzContext -Subscription $selection.Id
     Write-Host "You have selected a new context:" $selection.Name -ForegroundColor Yellow
 }
+# creates a master list (table) for storing all of the storage account properties
+[System.Collections.ArrayList]$list = New-Object -TypeName System.Collections.ArrayList
 
-# return all available storage accounts in the selected subscription
+# calls Azure RM and returns all storage accounts in the selected subscription
 $storageAccounts = Get-AzStorageAccount
-Write-Host "Retrieving and processing storage account properties..."
+Write-Host "Retrieving and processing storage account properties..." -ForegroundColor Cyan
 foreach ($sa in $storageAccounts) {
-    # skip all storage accounts that belong to the cloud-shell, webjobs, etc.
+    # skips all storage accounts connected to cloud-shell, webjobs, etc.
     if ($sa.StorageAccountName -notlike "webjob*" -and $sa.ResourceGroupName -notlike "cloud-shell-storage*") {
         Write-Host "Loading"$sa.StorageAccountName -NoNewline
         
         $row = New-Object PSObject
-        # populate basic storage information in the fields as row columns
+        # adding fields to a table row with basic storage account properties
         $row | Add-Member -MemberType NoteProperty -Name 'Subscription' -Value $context.Subscription.Name
         $row | Add-Member -MemberType NoteProperty -Name 'StorageAccount' -Value $sa.StorageAccountName
         $row | Add-Member -MemberType NoteProperty -Name 'ResouceGroup' -Value $sa.ResourceGroupName
 
-
-        #retrieve storage account tags (relevant ones)
+        # retrieves storage account tags and adds them as fields (columns)
         $acceptKeys = "company", "team"
         foreach ($key in $sa.Tags.Keys) {
             $value = $sa.Tags[$key]
@@ -107,64 +98,58 @@ foreach ($sa in $storageAccounts) {
                 $row | Add-Member -MemberType NoteProperty -Name $key -Value $value
             }
         }
-
-        #add additional storage account properties per row (change as needed)
+        # adding additional storage account properties as new fields (columns)
         $row | Add-Member -MemberType NoteProperty -Name 'Type' -Value $sa.Kind
         $row | Add-Member -MemberType NoteProperty -Name 'AccessTier' -Value $sa.AccessTier
         $row | Add-Member -MemberType NoteProperty -Name 'SKU' -Value $sa.Sku.Name
         $row | Add-Member -MemberType NoteProperty -Name 'Location' -Value $sa.PrimaryLocation
         
 
-        #call custom defined function to retrieve extended properties for each account
+        # calls a custom defined function to retrieve extended storage properties
         $ext = New-ExtendedStorageProps(Get-AzStorageBlobServiceProperty -StorageAccount $sa)
         $ext.PSObject.Properties | ForEach-Object {
             $row | Add-Member -MemberType NoteProperty -Name $_.Name -Value $_.Value
         }
 
-        # add property field related to network access and security boundaries
+        # adding fields related to network access and perimater security, etc.
         $row | Add-Member -MemberType NoteProperty -Name 'PublicNetAccess' -Value $sa.PublicNetworkAccess
         $row | Add-Member -MemberType NoteProperty -Name 'BlobPublicAccess' -Value $sa.AllowBlobPublicAccess
         $row | Add-Member -MemberType NoteProperty -Name 'AllowSharedKey' -Value $sa.AllowSharedKeyAccess
         $row | Add-Member -MemberType NoteProperty -Name 'AllowCrossTenant' -Value $sa.AllowCrossTenantReplication
 
-        # add row to the master list (table)
+        # adds the entire row to the master list (table)
         $list.Add($row) | Out-Null
         Write-Host "OK"
     }
 }
-# Count how many storage accounts where skipped
 $skipCount = $storageAccounts.Count-$list.Count
 Write-Host $list.Count"storage accounts processed. ($skipCount skipped)" -ForegroundColor Yellow
 
+# Presents the user with a choice of saving the results to a file or display on screen
 $key = Read-Host "- Save output to a file? Choose No to only show Gridview (Y/n)"
 if ($key -eq "Y") {
         
-    # Output table to excel file and display grid-view (make sure to include file-path and extension)
+    # Outputs table to a file (make sure to include filename and extension)
     $csvfile = ".\PSOutputFiles\StorageAccProps.csv"
     #$xlsfile = ".\PSOutputFiles\StorageAccProps.xlsx"
 
     try {
-        Write-Host "Writing file to disk..."
+        Write-Host "Writing file to disk..." -ForegroundColor Cyan
         $list | Export-Csv -Path $csvfile -Delimiter ";"
         #$list | Export-Excel -Path $xlsfile -WorksheetName "ExtendedProperties" -TableName "storageprops" -AutoSize
-        Write-Host "Success! Output can be found under the \PSOutputFiles folder" -ForegroundColor Green
+        Write-Host "Success! Output can be found under $csvfile" -ForegroundColor Green
     } catch {
         Write-Error $_.Exception.GetType().FullName
         Write-Host -ForegroundColor Yellow "Possible reason: File already open? (locked)"
-        break
+        $LASTEXITCODE = 1
     } finally {
         if ($LASTEXITCODE -eq 0) {
-            Write-Host -ForegroundColor Cyan "Script completed successfully."
+            Write-Host -ForegroundColor Green "Script completed successfully."
         } else {
-            Write-Host -ForegroundColor Blue "Script finished with exit code: $LASTEXITCODE"
+            Write-Host -ForegroundColor Cyan "Script finished with exit code: $LASTEXITCODE"
         }
     }
-
 } else {
-    if ($LASTEXITCODE -eq 0) {
-        $list | Out-GridView -Title "StorageAccountProperties"
-        Write-Host -ForegroundColor Cyan "Script completed successfully."
-    } else {
-        Write-Host -ForegroundColor Blue "Script finished with exit code: $LASTEXITCODE"
-    }  
+    $list | Out-GridView -Title "StorageAccountProperties"
+    Write-Host -ForegroundColor Green "Script completed successfully."
 }

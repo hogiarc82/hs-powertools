45360836593e42e7546f5777ad0438b8520288fd
Roman Castro <roman.castro@hogia.se>
2023-07-05 13:51:00 +0200

    added a few things, renamed some stuff

--- generate_comments.ps1
+++ generate_comments.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+    Purpose of this cmdlet is to generate a text file (md) with all code comments 
+.DESCRIPTION
+    The script returns all comments inside the code marked with a # sign. Then runs it through 
+    a text summary feature (in progress) and returns the final content.
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+.EXAMPLE
+    ./generate_comments.ps1 <filename> <[optional]out-file>
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+#>
+
 param(
     [Parameter(Mandatory=$true)]
     [string]$ScriptFile,

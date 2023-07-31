fcf5d550cbbc664324019367f64b30dda6949b98
Mazdak Jafari <mazdak.jafari@hogia.se>
2023-07-31 13:28:20 +0000

    Merged PR 96088: added comments
    Added comments to move-sqldb-resources.ps1

--- move-sqldb-resources.ps1
+++ move-sqldb-resources.ps1
@@ -1,5 +1,9 @@
 ## TODO: Create a script that moves a SQL database from one resource group to another
+
+# Creates a empty list to store objects
 $sites = @()
+
+# Creates a list of objects from Azure Resources and adds them in sites if succeded
 foreach ($i in $testdb) {
     $rgname = $i.DatabaseName+"qa"
     try {
@@ -8,6 +12,7 @@ foreach ($i in $testdb) {
     } catch {
     }
 }
+# Displays information from retrieved databases and simulating a move for each database
 foreach ($s in $sites) {
     $dbname = $s.Name.Substring(0, $s.Name.Length-2)
     try {
@@ -19,7 +24,7 @@ foreach ($s in $sites) {
     } catch {
     }
 }
-
+# Checks if varable is empty. If not, a table is displayed with number of matches and actions taken place
 if ($null -ne $sites) {
     $sites | Format-Table
     Write-Host "Found $($sites.Count) matches and moved them to another resourcegroup" -ForegroundColor Red
